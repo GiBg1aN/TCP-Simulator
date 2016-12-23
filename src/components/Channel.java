3f9722ff7db583924f40fb989ab865734ec20af6
hjorvardr <lorenzo.giudice92@gmail.com>
2016-12-23 23:50:45 +0100

    Conflicts solved.

--- src/components/Channel.java
+++ src/components/Channel.java
@@ -7,19 +7,13 @@ import static mainPackage.MyConstants.*;
 public class Channel extends LinkedList<MySegment> {
     private static final int MAX_LENGTH = T;
     private static final Channel instance = new Channel();
-<<<<<<< HEAD
+
     
     
     private Channel() {}
     
     public static Channel getInstance() { return instance; }
-=======
 
-
-    public Channel() {}
-
-    public static Channel getInstance() { return instance; } // Singleton
->>>>>>> origin/master
 
     public void enqueueSegment(MySegment segm) {
         if (size() < MAX_LENGTH) {
